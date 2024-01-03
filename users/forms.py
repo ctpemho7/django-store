a5fa91642d6f265866e8975eca8b28b8403bd073
Semyon Lukin <lukinsemyon@yandex.ru>
2024-01-03 14:26:21 +0500

    register form and controller done

--- users/forms.py
+++ users/forms.py
@@ -17,3 +17,34 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4",
+        'placeholder': 'Введите имя',
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4",
+        'placeholder': 'Введите фамилию',
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control py-4",
+        'placeholder': 'Введите логин',
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': "form-control py-4",
+        'placeholder': 'Введите адрес эл. почты',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4",
+        'placeholder': 'Введите пароль',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control py-4",
+        'placeholder': 'Подтвердите пароль',
+    }))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
@@ -23,4 +23,16 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 
+def register(request):
+    if request.method == 'POST':
+        data = request.POST
+        form = UserRegisterForm(data=data)
+        if form.is_valid():
+            # Сохраняем форму. Под капотом сам сохранит нужную модель
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegisterForm()
 
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
