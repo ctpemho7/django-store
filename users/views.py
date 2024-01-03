eeb048f9c5ab138af6761405803a7228e9af974e
Semyon Lukin <lukinsemyon@yandex.ru>
2024-01-03 16:13:15 +0500

    showing errors

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 
@@ -28,6 +28,7 @@ def register(request):
         data = request.POST
         form = UserRegisterForm(data=data)
         if form.is_valid():
+            messages.success(request, 'Вы успешно зарегистрировались!')
             # Сохраняем форму. Под капотом сам сохранит нужную модель
             form.save()
             return HttpResponseRedirect(reverse('users:login'))
