56711167c2388ea505223f676af3da00231afac5
Semyon Lukin <lukinsemyon@yandex.ru>
2024-01-06 00:17:47 +0500

    admin panel view changed

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 # Register your models here. (отображение таблиц в админке)
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name', '-price', 'quantity')
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
--- products/models.py
+++ products/models.py
@@ -10,6 +10,10 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории продуктов'
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
@@ -21,6 +25,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
 
 class BasketQuerySet(models.QuerySet):
